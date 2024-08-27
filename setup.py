5b95c24d19203a294b128f0ec9daaad2cc621e11
liebe-developing <liebedeveloping69@gmail.com>
2024-08-27 12:06:14 +0330

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="shamsi_datetime",
-    version="0.1.0", 
+    version="0.1.3", 
     description="A Shamsi (Jalali) datetime class for NumPy integration",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
